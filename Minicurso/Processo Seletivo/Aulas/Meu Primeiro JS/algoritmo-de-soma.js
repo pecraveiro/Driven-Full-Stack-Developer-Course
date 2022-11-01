ff2ba2e29f84e94e31e6a191a5e270559a47fe72
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-01 02:03:49 -0300

    Algoritmo de soma de dois números

---
+++ Minicurso/Processo Seletivo/Aulas/Meu Primeiro JS/algoritmo-de-soma.js
@@ -0,0 +1,13 @@
+// 01/11/2022 @ pedro craveiro - bolsista driven
+// site recomendado: replit.com/languages/nodejs
+
+function somar(num1, num2){
+
+  // criação de variável
+  let resultado = num1 + num2; //somando os 2 parâmetros
+  return resultado; // retorna a soma
+}
+//somar(2,10)
+
+// A função somar recebe os parâmetros num1 e num2
+// let => somente na hora de criar variáveis
