da082668ce005c9ba99dbf08e7ce0cf919ce5283
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-01 02:19:54 -0300

    Create mediaSimples.js
    Implemente a função ao lado, que calcula a média simples entre 2 números.

---
+++ Minicurso/Processo Seletivo/Aulas/Calculando com JS/Exercício/mediaSimples.js
@@ -0,0 +1,7 @@
+// Implemente a função ao lado, que calcula a média simples entre 2 números
+
+function media(num1, num2) {
+ let media = (num1 + num2) / 2 // Escreva aqui seu código
+
+return media;
+}
