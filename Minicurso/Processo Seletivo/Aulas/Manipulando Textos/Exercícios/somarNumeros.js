2ee191c25e30a6c3d0d1c87cf4164183eebb66cf
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-01 02:25:24 -0300

    Create somarNumeros.js

---
+++ Minicurso/Processo Seletivo/Aulas/Manipulando Textos/Exercícios/somarNumeros.js
@@ -0,0 +1,6 @@
+// Implemente a função ao lado, que soma dois números e retorna o texto A soma deu: x, sendo x o resultado da soma
+
+function somar(num1, num2) {
+  let soma = num1 + num2;
+  return "A soma deu: " + soma;
+}
