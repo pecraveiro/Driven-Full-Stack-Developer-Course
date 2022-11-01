278d1fb88da6c4a165571c6eb3b1b331b2793f31
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-01 02:33:41 -0300

    Create maiordeIdade.js
    Implemente a função ao lado, que retorna Maior de idade caso a idade passada seja maior que 17 ou Menor de idade caso contrário.

---
+++ Minicurso/Processo Seletivo/Aulas/Condicionais/Exercícios/maiordeIdade.js
@@ -0,0 +1,15 @@
+// Implemente a função ao lado, que retorna Maior de idade caso a idade passada seja maior que 17 ou Menor de idade caso contrário.
+
+function maiorDeIdade(idade) {
+
+ let maior = "Maior de idade";
+ let menor = "Menor de idade";
+
+if(idade > 17) 
+{
+ return maior;
+}
+else {
+  return menor;
+  }
+}
