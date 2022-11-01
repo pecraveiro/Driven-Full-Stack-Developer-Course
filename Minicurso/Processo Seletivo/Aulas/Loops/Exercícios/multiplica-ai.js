3604f09e38b3dad41c9dabb300cdde93374c09f7
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-01 20:39:41 -0300

    Create multiplica-ai.js

---
+++ Minicurso/Processo Seletivo/Aulas/Loops/Exercícios/multiplica-ai.js
@@ -0,0 +1,9 @@
+function multiplica(texto, repeticoes) {
+  // Escreva aqui seu código
+  let resultado = "";
+
+  for (i = 0; i < repeticoes; i++) {
+    resultado += texto;
+  }
+  return resultado;
+}
