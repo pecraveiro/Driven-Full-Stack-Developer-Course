caf10831f6cb6b9e868ef84878f11222759a6584
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-01 20:17:58 -0300

    Create exemplo-de-aula.js

---
+++ Minicurso/Processo Seletivo/Aulas/Loops/exemplo-de-aula.js
@@ -0,0 +1,19 @@
+// exemplos da aula
+
+function umbridge() {
+let contador = 0; // inicia com o valor 0
+let frase = "";
+
+// esse código repete 100 vezes
+while(contador < 100) {
+	frase += "Não devo contar mentiras - Bart Simpsons\n";
+  contador++;
+	// contador++
+	// estamos incrementando o contador, no caso, a variável criada
+}
+return frase;
+}
+
+// diferenca entre while e if
+// while fica repetindo a condicao
+// if passa pela condição uma unica vez
