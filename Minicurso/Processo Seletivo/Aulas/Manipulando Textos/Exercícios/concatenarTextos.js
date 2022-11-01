190a75a914ac3d84b418082ac4eb2f4bd2f9e51f
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-01 02:24:46 -0300

    Create concatenarTextos.js

---
+++ Minicurso/Processo Seletivo/Aulas/Manipulando Textos/Exercícios/concatenarTextos.js
@@ -0,0 +1,6 @@
+// Implemente a função ao lado, que retorna a concatenação dos dois parâmetros que ela recebe
+
+function concatenar(texto1, texto2) {
+
+return texto1 + texto2;// Escreva aqui seu código
+}
