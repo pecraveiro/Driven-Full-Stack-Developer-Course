c73e886f6942ccecb53d5bedf0c1b9147303577b
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-03 00:22:07 -0300

    Create lista-de-nomes.js
    Implemente a função ao lado que recebe 3 nomes e retorna esses nomes em uma array.

---
+++ Minicurso/Processo Seletivo/Aulas/Arrays/Exercícios/lista-de-nomes.js
@@ -0,0 +1,13 @@
+//Implemente a função ao lado que recebe 3 nomes e retorna esses nomes em uma array.
+
+function listar(nome1, nome2, nome3) {
+let nomes = ["Pedro", "Maite", "Victor" ];
+return nomes;
+}
+
+/*
+function listar(nome1, nome2, nome3) {
+let nomes = ["joão", "maria", "josé" ];
+return nomes;
+}
+*/
