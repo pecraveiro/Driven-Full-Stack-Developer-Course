e3b7dc96235f634533c29c9dd6036226df8fafef
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-03 00:05:13 -0300

    Create informacoes-da-aula01.js

---
+++ Minicurso/Processo Seletivo/Aulas/Arrays/informacoes-da-aula01.js
@@ -0,0 +1,8 @@
+// lista de informações -> array[];
+// let alunos = ["Ana, "Bia", "Carlos"];
+
+function teste() {
+let alunos = ["Ana", "Bia", "Carlos"];
+return alunos[2]; //carlos
+//return alunos.length; //-> 3
+}
