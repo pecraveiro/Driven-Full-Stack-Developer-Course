877c85a145441e247a9561fb6014d08d07260912
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-03 00:11:16 -0300

    Create informacoes-da-aula02.js

---
+++ Minicurso/Processo Seletivo/Aulas/Arrays/informacoes-da-aula02.js
@@ -0,0 +1,12 @@
+function teste() {
+let alunos = ["Ana", "Bia", "Carlos"];
+let texto = "";
+
+	for(let contador = 0; contador < alunos.length; contador++) {
+	alunos[contador];
+	texto += alunos[contador];
+	}
+	return texto;
+}
+
+//teste();
