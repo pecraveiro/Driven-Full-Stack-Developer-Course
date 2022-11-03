7f1b136656b749c809194a4ff28f7fad0051b197
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-11-03 00:53:15 -0300

    Create Desafio01.js

---
+++ Minicurso/Processo Seletivo/Desafio/Desafio01.js
@@ -0,0 +1,9 @@
+// Bem vindo
+// Implemente a função ao lado, que retorna Bem-vindo(a) Fulano, sendo Fulano o nome passado por parâmetro.
+// Exemplo: deve retornar "Bem-vindo(a) João" quando passado nome = "João"
+
+function bemVindo (nome) {
+  // escreva o código aqui :)
+  let greetings = "Bem-vindo(a) " + nome;
+  return greetings;
+}
